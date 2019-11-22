221debfbaed83cafa49c2567bc74b6a1d7319050
hostproject <v.davydenko.o@ya.ru>
2019-11-22 20:23:14 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,12 +1,17 @@
 <?php
-	$f = fopen(__DIR__ . "/log.log", "a");
+$f = fopen(__DIR__ . "/log.log", "a");
+$out = shell_exec("git -C /root/TestV/ pull");
+
+print_r($out);
+if(stripos($out, "updating") === true){
+	print_r("print: true");
 	$out = shell_exec("git -C /root/TestV/ pull");
-	print_r(" print: ". $out);
-	if(strrpos($out, "error: Your local") === true){
+	if(stripos($out, "Updating") === true){
 		shell_exec("git -C /root/TestV/ reset --hard");
 		$out = shell_exec("git -C /root/TestV/ pull");
 	}
-	
-	fwrite($f, "\r\n" . $out);
-	fclose($f);
+}
+
+fwrite($f, "\r\n" . $out);
+fclose($f);
 ?>
